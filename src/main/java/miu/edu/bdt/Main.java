e1fe1e21d6674fc1ecb7dcfc27dc5e2d81714a2a
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-04-03 11:07:06 -0500

    lesson 1

--- src/main/java/miu/edu/bdt/Main.java
+++ src/main/java/miu/edu/bdt/Main.java
@@ -2,6 +2,8 @@ package miu.edu.bdt;
 
 public class Main {
     // docker run --name=quickstart.cloudera --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p 7180:7180 -p 80:80 cloudera/quickstart:latest /usr/bin/docker-quickstart -d
+    // sudo /home/cloudera/cloudera-manager --express --force
+    // sudo service hue start
     public static void main(String[] args) {
         System.out.println("Welcome to Big Data Technology");
     }

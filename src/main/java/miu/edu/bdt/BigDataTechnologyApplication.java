b98172981bd48c437cb9458e6c9083398d8445f8
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-04-05 14:09:35 -0500

    lesson 3

--- src/main/java/miu/edu/bdt/BigDataTechnologyApplication.java
+++ src/main/java/miu/edu/bdt/BigDataTechnologyApplication.java
@@ -1,6 +1,6 @@
 package miu.edu.bdt;
 
-public class Main {
+public class BigDataTechnologyApplication {
     // docker run --name=quickstart.cloudera --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p 7180:7180 -p 80:80 cloudera/quickstart:latest /usr/bin/docker-quickstart -d
     // sudo /home/cloudera/cloudera-manager --express --force
     // sudo service hue start
--- src/main/java/miu/edu/bdt/lesson3/WordCountMapper.java
+++ src/main/java/miu/edu/bdt/lesson3/WordCountMapper.java
@@ -5,12 +5,30 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class Main {
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper {
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private final Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
@@ -19,8 +37,8 @@ public class Main {
             System.exit(2);
         }
         Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(WordCount.TokenizerMapper.class);
+        job.setJarByClass(WordCountMapper.class);
+        job.setMapperClass(WordCountMapper.TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);

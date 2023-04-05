1d5e1a00bbfcd8a1f8d7b91fb267f437d8fdcdf3
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-04-05 13:47:47 -0500

    lesson 3

---
+++ src/main/java/miu/edu/bdt/lesson3/WordCount.java
@@ -0,0 +1,24 @@
+package miu.edu.bdt.lesson3;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCount {
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private final Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+}

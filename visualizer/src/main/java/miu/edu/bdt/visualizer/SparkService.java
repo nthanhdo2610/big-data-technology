19549b411a256e0c494bd6e491ca6402df1a8f13
TrauNguyen <nguyenvanngoctrau@gmail.com>
2023-04-25 07:54:58 -0500

    commit spark sql and hive

--- visualizer/src/main/java/miu/edu/bdt/visualizer/SparkService.java
+++ visualizer/src/main/java/miu/edu/bdt/visualizer/SparkService.java
@@ -1,30 +1,31 @@
 package miu.edu.bdt.visualizer;
 
+import java.util.List;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SparkService {
 
-    private static SparkConf sparkConf = null;
+    private static SparkSession sparkSession = null;
     private static SparkService INSTANCE = null;
-    private static final Logger logger = LoggerFactory.getLogger(SparkService.class);
 
     private SparkService() {
-        sparkConf = new SparkConf();
-        sparkConf.setMaster("local[*]");
-        sparkConf.set("hive.metastore.uris", Constant.THRIFT_CONNECTION)
-                .set("hive.metastore.warehouse.dir", "/user/hive/warehouse")
-                .set("spark.sql.warehouse.dir", "/user/hive/warehouse")
-                .set("hive.exec.scratchdir", "/tmp/the-current-user-has-permission-to-write-in")
-                .set("spark.yarn.security.credentials.hive.enabled", "true")
-                .set("spark.sql.hive.metastore.jars", "maven")
-                .set("spark.sql.hive.metastore.version", "1.2.1")
-                .set("spark.sql.catalogImplementation", "hive")
-        ;
+    	final SparkConf sparkConf = new SparkConf();
+    	
+    	sparkConf.setMaster("local");
+        sparkConf.set("hive.metastore.uris", Constant.THRIFT_CONNECTION);
+        sparkConf.set("hive.exec.scratchdir", "/tmp/the-current-user-has-permission-to-write-in");
+        
+        sparkSession = SparkSession
+        		.builder()
+        		.appName("Spark SQL-Hive")
+        		.config(sparkConf)
+        		.enableHiveSupport()
+        		.getOrCreate();
     }
 
     public static SparkService getInstance() {
@@ -33,28 +34,48 @@ public class SparkService {
         }
         return INSTANCE;
     }
+    
+    public List<Weather> getWeatherIOWA() {
+		sparkSession.sql(String.format(Constant.DROP_TABLE_SQL, "bdt_iowa_weather"));
+		Dataset<Row> sqlDF = sparkSession.sql(String.format(Constant.SELECT_WEATHER_IOWA_SQL, Constant.TABLE_NAME));
+		sqlDF.write().saveAsTable("bdt_iowa_weather");
+		sqlDF.show();
+		
+		List<Weather> list = sqlDF.as(Encoders.bean(Weather.class)).collectAsList();		
+		return list;
+	}
+	
+	public List<AvgTemperature> getLast7DaysAvgTempByArea() {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT zip_code, city, AVG(temperature) AS avgTemp")
+			.append(" FROM ")
+			.append(Constant.TABLE_NAME)
+			.append(" WHERE updated_date >= DATE(NOW()) + INTERVAL -7 DAY")
+			.append(" GROUP BY zip_code, city");
+		
+		sparkSession.sql(String.format(Constant.DROP_TABLE_SQL, "bdt_avg_weather"));
+		Dataset<Row> sqlDF = sparkSession.sql(sql.toString());
+		sqlDF.write().saveAsTable("bdt_avg_weather");
+		sqlDF.show();
 
-    public void updateView() {
-        logger.info("Spark SQL-Hive Update View!!!");
-        try (SparkSession session = SparkSession
-                .builder()
-                .appName("Spark SQL-Hive")
-                .config(sparkConf)
-                .enableHiveSupport()
-                .getOrCreate()) {
-            session.sql(String.format(Constant.DROP_TABLE_SQL, Constant.IOWA_TABLE_NAME));
-            Dataset<Row> iowa = session.sql(String.format(Constant.SELECT_WEATHER_IOWA_SQL, Constant.TABLE_NAME));
-            iowa.write().saveAsTable(Constant.IOWA_TABLE_NAME);
-
-            // test show top result
-            session.sql(Constant.SELECT_LIMIT_10_WEATHER_IOWA_SQL).show();
-        } catch (Exception e) {
-            logger.error("Spark SQL-Hive Update View Got Error: " + e.getMessage());
-            e.printStackTrace();
-            System.exit(0);
-        }
-
-        logger.info("Spark SQL-Hive Update View Successfully!!!");
-    }
-
+		List<AvgTemperature> list = sqlDF.as(Encoders.bean(AvgTemperature.class)).collectAsList();
+		return list;
+	}
+	
+	public List<AvgTemperature> getHotAreaWithTempGreaterThan83() {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT zip_code, city, temperature as hotTemp")
+	        .append(" FROM ")
+	        .append(Constant.TABLE_NAME)
+	        .append(" WHERE temperature > 83")
+	        .append(" GROUP BY zip_code, city, temperature");
+	        
+		sparkSession.sql(String.format(Constant.DROP_TABLE_SQL, "bdt_hot_weather"));
+		Dataset<Row> sqlDF = sparkSession.sql(sql.toString());
+		sqlDF.write().saveAsTable("bdt_hot_weather");
+		sqlDF.show();
+		
+		List<AvgTemperature> list = sqlDF.as(Encoders.bean(AvgTemperature.class)).collectAsList();
+		return list;
+	}
 }
